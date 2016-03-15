9ae38b162564ed192a13c4ae916d75ed370fb399
Auke Kok <sofar@foo-projects.org>
2016-03-14 22:14:46 -0700

    Add a manual /inspect x y z chatcommand
    Allows to inspect hidden and untargetable nodes, as well
    as random nodes all over the map.

--- init.lua
+++ init.lua
@@ -10,6 +10,49 @@ of the license, or (at your option) any later version.
 
 --]]
 
+local function inspect_pos(pos)
+	local node = minetest.get_node(pos)
+	local desc = "===== node data =====\n"
+	desc = desc .. "name = " .. node.name .. "\n"
+	desc = desc .. "param1 = " .. node.param1 .. "\n"
+	desc = desc .. "param2 = " .. node.param2 .. "\n"
+	local light = minetest.get_node_light({x = pos.x, y = pos.y + 1, z = pos.x}, nil)
+	desc = desc .. "light = " .. light .. "\n"
+
+	local timer = minetest.get_node_timer(pos)
+	if timer:get_timeout() ~= 0 then
+	desc = desc .. "==== node timer ====\n"
+	desc = desc .. "timeout = " .. timer:get_timeout() .. "\n"
+	desc = desc .. "elapsed = " .. timer:get_elapsed() .. "\n"
+	end
+
+	local nodedef = minetest.registered_items[node.name]
+	if nodedef then  -- Some built in nodes have no nodedef
+		desc = desc .. "==== nodedef ====\n"
+		desc = desc .. dump(nodedef) .. "\n"
+	end
+	local meta = minetest.get_meta(pos)
+	local table = meta:to_table()
+	local fields = minetest.serialize(table.fields)
+	desc = desc .. "==== meta ====\n"
+	desc = desc .. "meta.fields = " .. fields .. "\n"
+	desc = desc .. "\n"
+	local inventory = meta:get_inventory()
+	desc = desc .. "meta.inventory = \n"
+	for key, list in pairs(inventory:get_lists()) do
+		desc = desc .. key .. " : \n"
+		local size = #list
+		for i = 1, size do
+			local stack = list[i]
+			if not stack:is_empty() then
+				desc = desc .. "\"" .. stack:get_name() .. "\" - " .. stack:get_count() .. "\n"
+			end
+		end
+	end
+
+	return desc
+end
+
 minetest.register_tool("inspector:inspector", {
 	description = "Inspector Tool",
 	inventory_image = "inspector.png",
@@ -28,52 +71,8 @@ minetest.register_tool("inspector:inspector", {
 			if pointed_thing.type ~= "node" then
 				desc = "..."
 			else
-				local node = minetest.get_node(pos)
-				desc = "===== node data =====\n"
-				desc = desc .. "name = " .. node.name .. "\n"
-				desc = desc .. "param1 = " .. node.param1 .. "\n"
-				desc = desc .. "param2 = " .. node.param2 .. "\n"
-				local light = minetest.get_node_light(pointed_thing.above, nil)
-				desc = desc .. "light = " .. light .. "\n"
-
-				local timer = minetest.get_node_timer(pos)
-				if timer:get_timeout() ~= 0 then
-				desc = desc .. "==== node timer ====\n"
-				desc = desc .. "timeout = " .. timer:get_timeout() .. "\n"
-				desc = desc .. "elapsed = " .. timer:get_elapsed() .. "\n"
-				end
-
-				local nodedef = minetest.registered_items[node.name]
-				if nodedef then  -- Some built in nodes have no nodedef
-					desc = desc .. "==== nodedef ====\n"
-					desc = desc .. dump(nodedef) .. "\n"
-				end
-				local meta = minetest.get_meta(pos)
-				local table = meta:to_table()
-				local fields = minetest.serialize(table.fields)
-				desc = desc .. "==== meta ====\n"
-				desc = desc .. "meta.fields = " .. fields .. "\n"
-				desc = desc .. "\n"
-				local inventory = meta:get_inventory()
-				desc = desc .. "meta.inventory = \n"
-				for key, list in pairs(inventory:get_lists()) do
-					desc = desc .. key .. " : \n"
-					local size = #list
-					for i = 1, size do
-						local stack = list[i]
-						if not stack:is_empty() then
-							desc = desc .. "\"" .. stack:get_name() .. "\" - " .. stack:get_count() .. "\n"
-						end
-					end
-				end
+				desc = inspect_pos(pos)
 			end
-
-			local timer = minetest.get_node_timer(pos)
-			if timer:is_started() then
-				 desc = desc .. "timer.timeout = " .. timer:get_timeout() .. "\n"
-				 desc = desc .. "timer.elapsed = " .. timer:get_elapsed() .. "\n"
-			end
-
 
 		elseif pointed_thing.type == "object" then
 
@@ -93,3 +92,21 @@ minetest.register_tool("inspector:inspector", {
 	end,
 })
 
+minetest.register_chatcommand("inspect", {
+	params = "inspect",
+	description = "inspect a pos",
+	privs = {server = true},
+	func = function(name, param)
+		local paramlist = string.split(param, " ")
+		local pos = {x = paramlist[1], y = paramlist[2], z = paramlist[3]}
+		local desc = inspect_pos(pos)
+		local formspec = "size[12,8]"..default.gui_bg.. -- default.gui_bg_img..
+							 "label[0.5,0.5;Node Information]"..
+							 "textarea[0.5,1.5;11.5,7;text;Contents:;"..
+							 minetest.formspec_escape(desc).."]"..
+							 "button_exit[2.5,7.5;3,1;close;Close]"
+
+		minetest.show_formspec(name, "default:team_choose", formspec)
+		return true
+	end,
+})
